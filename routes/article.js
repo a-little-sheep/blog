7e2cbb6ac501c1cfaaf6f6e43b926a836b1debba
yangyan <yangyan@rimag.com.cn>
2017-07-11 17:06:27 +0800

    博客完成

--- routes/article.js
+++ routes/article.js
@@ -7,7 +7,7 @@ router.get('/add',checkLogin,function (req,res) {
       if(err){
          res.send('获取文章分类失败');
       }else{
-         res.render('article/add',{title:'添加文章',categories});
+         res.render('article/add',{title:'添加文章',categories,article:{}});
       }
    });
 });
@@ -26,12 +26,41 @@ router.post('/add',checkLogin,function (req,res) {
 router.get('/detail/:id',checkLogin,function(req,res) {
    let id = req.params.id;
    Article.findById(id).populate('category').exec(function (err,article) {
-      res.render('article/detail',{title:'文章编辑',article});
+      res.render('article/detail',{title:'文章预览',article});
    });
 });
 //文章编辑
 router.get('/update/:id',function (req,res) {
    let id = req.params.id;
-   Article.f
+   Category.find({},function (err,categories) {
+        Article.findById(id,function (err,article) {
+           if(err)
+               return res.back(err);
+            res.render('article/add',{title:'文章编辑',categories,article});
+        });
+   });
 });
+//文章编辑提交
+router.post('/update/:id',function (req,res) {
+   let _id = req.params.id;
+   let article = req.body;
+   Article.update({_id},article,function (err,result) {
+      if(err){
+          res.back(err);
+      }else{
+          res.success('文章编辑成功',`/article/detail/${_id}`);
+      }
+   });
+});
+//文章删除
+router.get('/delete/:id',function (req,res) {
+    let _id = req.params.id;
+    Article.remove({_id},function (err,result) {
+        if(err){
+            res.back(err);
+        }else{
+            res.success('该文章删除成功','/');
+        }
+    })
+})
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ let router = express.Router();//路由
 let {Article} = require('../model');
 let {checkLogin} = require('../authware');
 router.get('/',checkLogin,function (req,res) {
-    let author = req.session.user._id;
-    Article.find({author:author}).populate('author').exec(function (err,articles) {
+    //let author = req.session.user._id;
+    Article.find({}).populate('author').exec(function (err,articles) {
         console.log(articles);
         res.render('index',{title:'首页',articles});
     });
